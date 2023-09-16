2f2a6a1f4595988d6155da492ef8d9e339d736c6
Joshua Duplaa <joshuaduplaa@outlook.com>
2023-09-16 10:27:16 -0500

    TDS script Baud Speed

---
+++ TDS_script/TDS_script.ino
@@ -0,0 +1,11 @@
+const int TDS_PIN = 34; // ADC pin where the TDS sensor is connected
+float tdsValue = 0;
+
+void setup() {
+  Serial.begin(115200);
+  //Very Fast Baud Rate
+}
+
+void loop() {
+  
+}

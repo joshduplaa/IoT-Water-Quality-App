9c7b44b8b14fe683bd219780a5d227b22e54ead2
Joshua Duplaa <joshuaduplaa@outlook.com>
2023-09-16 17:12:43 -0500

    finished TDS script

--- TDS_script/TDS_script.ino
+++ TDS_script/TDS_script.ino
@@ -21,8 +21,7 @@ dex.php/Gravity:_Analog_TDS_Sensor_/_Meter_For_Arduino_SKU:_SEN0244>
 #define VREF 5.0 // analog reference voltage(Volt) of the ADC
 #define SCOUNT 30 // sum of sample point
 
-int analogBuffer[SCOUNT]; // store the analog value in the array, read
-from ADC
+int analogBuffer[SCOUNT]; // store the analog value in the array, read from ADC
 int analogBufferTemp[SCOUNT];
 int analogBufferIndex = 0,copyIndex = 0;
 float averageVoltage = 0,tdsValue = 0,temperature = 25;
@@ -54,6 +53,7 @@ void loop()
     float compensationCoefficient=1.0+0.02*(temperature-25.0); //temperature compensation formula: fFinalResult(25^C) = fFinalResult(current)/(1.0+0.02*(fTP-25.0));
     float compensationVolatge=averageVoltage/compensationCoefficient; //temperature compensation
     tdsValue = (133.42*compensationVolatge*compensationVolatge*compensationVolatge - 255.86*compensationVolatge*compensationVolatge + 857.39*compensationVolatge)*0.5; //convert voltage value to tds value
+ 
     //Serial.print("voltage:");
     //Serial.print(averageVoltage,2);
     //Serial.print("V ");
